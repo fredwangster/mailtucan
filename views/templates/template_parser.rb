b93838a74368ab6a63d4db72e32e0ccf1af8f3b9
Vincent Sanchez-Gomez <vincent@pagevamp.com>
2013-09-21 11:38:19 -0400

    Updated template 1 and 2 and also edited template parser

--- views/templates/template_parser.rb
+++ views/templates/template_parser.rb
@@ -4,10 +4,17 @@ helpers do
 		#do stuff to parse data here
 		pageData["thatsme"] = "fred"
 
-		pageData["feed"]["data"].each do |feed|
-
-			feed["picture"] = feed["picture"].gsub("_s", "_n")
-
+		pageData["posts"]["data"].each do |feed|
+			if feed["picture"]
+				feed["picture"] = feed["picture"].gsub("_s", "_n")
+			end
+			feed["created_time"] = Date.parse(feed["created_time"]).strftime("%b %d, %Y")
+		end
+		
+		pageData["events"]["data"].each do |event|
+			if event["start_time"]
+				event["start_time"] = Date.parse(event["start_time"]).strftime("%b %d, %Y %H:%m:%S%p")
+			end
 		end
 
 		return pageData
